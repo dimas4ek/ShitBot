47b0d691264a3b7f48813cdb369262daa21d5d30
Dmitry Gulyaev <dimas4ek@gmail.com>
2023-03-23 23:11:20 +0500

    added malicious links detector

---
+++ src/main/java/org/shithackers/listeners/LinksListener.java
@@ -0,0 +1,70 @@
+package org.shithackers.listeners;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import okhttp3.*;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LinksListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String messageContent = event.getMessage().getContentRaw();
+        Pattern pattern = Pattern.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\))))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\))))*\\b)");
+        Matcher matcher = pattern.matcher(messageContent);
+
+        if (matcher.find()) {
+            OkHttpClient client = new OkHttpClient();
+
+            String url = "https://www.virustotal.com/api/v3/urls";
+            String apiKey = "cb5d33034e119e18614b7fb35d364f54850e891944cacf7aa93c9ea56007e5bf";
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("url", matcher.group())
+                .build();
+
+            Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .header("x-apikey", apiKey)
+                .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful()) {
+                    String responseBody = response.body().string();
+                    JSONObject json = new JSONObject(responseBody);
+                    String dataId = json.getJSONObject("data").getString("id");
+                    String dataUrl = "https://www.virustotal.com/api/v3/analyses/" + dataId;
+
+                    Request request2 = new Request.Builder()
+                        .url(dataUrl)
+                        .header("x-apikey", apiKey)
+                        .build();
+
+                    try (Response response2 = client.newCall(request2).execute()) {
+                        if (response2.isSuccessful()) {
+                            String responseBody2 = response2.body().string();
+                            JSONObject json2 = new JSONObject(responseBody2);
+                            int sus = json2.getJSONObject("data").getJSONObject("attributes").getJSONObject("stats").getInt("suspicious");
+                            int mal = json2.getJSONObject("data").getJSONObject("attributes").getJSONObject("stats").getInt("malicious");
+
+                            if (mal > 2) {
+                                event.getMessage().reply("this site is malicious").queue();
+                            } else if (sus > 0) {
+                                event.getMessage().reply("this site is suspicious").queue();
+                            }
+                        } else {
+                            System.out.println("Error: " + response2.code() + " " + response2.message());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- src/main/java/org/shithackers/utils/RegisterCommands.java
+++ src/main/java/org/shithackers/utils/RegisterCommands.java
@@ -1,21 +1,26 @@
 package org.shithackers.utils;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import org.shithackers.commands.*;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 import org.shithackers.commands.info.ServerInfoCommand;
 import org.shithackers.commands.info.UserInfoCommand;
 import org.shithackers.commands.mod.BanUserCommand;
 import org.shithackers.commands.mod.MuteUserCommand;
+import org.shithackers.commands.mod.ReportCommand;
 import org.shithackers.commands.mod.WarnCommand;
-import org.shithackers.commands.other.WeatherCommand;
+import org.shithackers.commands.music.MusicCommand;
+import org.shithackers.commands.other.*;
+import org.shithackers.listeners.LinksListener;
+import org.shithackers.listeners.MemberMusicListener;
 import org.shithackers.listeners.UserJoinLeaveListener;
 import org.shithackers.listeners.MessagesListener;
-import org.shithackers.listeners.VerifyListener;
 
 public class RegisterCommands {
-    public void register(JDA api) {
+    public static void register(JDA api) {
         api.addEventListener(
             new UserInfoCommand(),
             new ServerInfoCommand(),
@@ -27,61 +32,79 @@ public class RegisterCommands {
             new ReportCommand(),
             new WeatherCommand(),
             new MessagesListener(),
-            new PrintQuoteCommand()
+            new PrintQuoteCommand(),
+            new AvatarCommand(),
+            new MusicCommand(),
+            new MemberMusicListener(),
+            new TestCommand(),
+            new LinksListener()
             //new VerifyListener() //в разработке
         );
 
         api.updateCommands().addCommands(
-                Commands.slash("userinfo", "Get info about a user")
-                    .addOption(OptionType.USER, "user", "The user to get info about", true),
+            Commands.slash("test", "test"),
 
-                Commands.slash("serverinfo", "Get info about the server"),
+            Commands.slash("play", "Play a song")
+                .addOption(OptionType.STRING, "link", "Insert youtube link here", false),
+            Commands.slash("skip", "Skip the current song"),
+            Commands.slash("stop", "Stop the music"),
 
-                Commands.slash("banuser", "Ban a user")
-                    .addOption(OptionType.USER, "user", "The user to ban", true)
-                    .addOption(OptionType.INTEGER, "days", "The amount of days to delete messages for", false)
-                    .addOption(OptionType.STRING, "reason", "The reason for the ban", false),
-                Commands.slash("unbanuser", "Unban a user")
-                    .addOption(OptionType.USER, "user", "The user to unban", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the unban", false),
+            Commands.slash("avatar", "Get the avatar of a user")
+                .addOption(OptionType.USER, "user", "The user to get the avatar of", false),
 
-                Commands.slash("mute", "Mute a user")
-                    .addOption(OptionType.USER, "user", "The user to mute", true)
-                    .addOption(OptionType.STRING, "duration", "The duration of the mute\nFor example: 5s, 15m, 1h", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the mute", false),
-                Commands.slash("unmute", "Unmute a user")
-                    .addOption(OptionType.USER, "user", "The user to unmute", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the unmute", false),
+            Commands.slash("userinfo", "Get info about a user")
+                .addOption(OptionType.USER, "user", "The user to get info about", true),
 
-                Commands.slash("warn", "Warn a user")
-                    .addOption(OptionType.USER, "user", "The user to warn", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the warn", false),
-                Commands.slash("unwarn", "Unwarn a user")
-                    .addOption(OptionType.USER, "user", "The user to unwarn", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the unwarn", false),
-                Commands.slash("clearwarns", "Clear all warns of a user")
-                    .addOption(OptionType.USER, "user", "The user to clear warns of", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the clear", false),
-                Commands.slash("warns", "Get the warns of a user")
-                    .addOption(OptionType.USER, "user", "The user to get the warns of", true),
-                Commands.slash("warnlist", "Get the warns of a user"),
+            Commands.slash("serverinfo", "Get info about the server"),
 
-                Commands.slash("set-welcome-channel", "Set the welcome channel")
-                    .addOption(OptionType.CHANNEL, "channel", "The channel to set as the welcome channel", true),
+            Commands.slash("ban", "Ban a user")
+                .addOption(OptionType.USER, "user", "The user to ban", true)
+                .addOption(OptionType.INTEGER, "days", "The amount of days to delete messages for", false)
+                .addOption(OptionType.STRING, "reason", "The reason for the ban", false),
+            Commands.slash("unban", "Unban a user")
+                .addOption(OptionType.USER, "user", "The user to unban", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the unban", false),
 
-                Commands.slash("set-moderation-channel", "Set the moderation channel")
-                    .addOption(OptionType.CHANNEL, "channel", "The channel to set as the moderation channel", true),
-                Commands.slash("report", "Report a user")
-                    .addOption(OptionType.USER, "user", "The user to report", true)
-                    .addOption(OptionType.STRING, "reason", "The reason for the report", false),
+            Commands.slash("mute", "Mute a user")
+                .addOption(OptionType.USER, "user", "The user to mute", true)
+                .addOption(OptionType.STRING, "duration", "The duration of the mute\nFor example: 5s, 15m, 1h", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the mute", false),
+            Commands.slash("unmute", "Unmute a user")
+                .addOption(OptionType.USER, "user", "The user to unmute", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the unmute", false),
 
-                Commands.slash("weather", "Get the weather of a city")
-                    .addOption(OptionType.STRING, "city", "The city to get the weather of", true),
+            Commands.slash("warn", "Warn a user")
+                .addOption(OptionType.USER, "user", "The user to warn", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the warn", false),
+            Commands.slash("unwarn", "Unwarn a user")
+                .addOption(OptionType.USER, "user", "The user to unwarn", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the unwarn", false),
+            Commands.slash("clearwarns", "Clear all warns of a user")
+                .addOption(OptionType.USER, "user", "The user to clear warns of", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the clear", false),
+            Commands.slash("warns", "Get the warns of a user")
+                .addOption(OptionType.USER, "user", "The user to get the warns of", true),
+            Commands.slash("warnlist", "Get the warns of a user"),
 
-                Commands.slash("quoterandom", "Get a random quote"),
-                Commands.slash("quotestart", "Start a quote")
-                    .addOption(OptionType.CHANNEL, "channel", "The channel to start the quote in", true),
-                Commands.slash("quotestop", "Stop a quote"))
-            .queue();
+            Commands.slash("set-welcome-channel", "Set the welcome channel")
+                .addOption(OptionType.CHANNEL, "channel", "The channel to set as the welcome channel", true),
+
+            Commands.slash("set-moderation-channel", "Set the moderation channel")
+                .addOption(OptionType.CHANNEL, "channel", "The channel to set as the moderation channel", true),
+            Commands.slash("report", "Report a user")
+                .addOption(OptionType.USER, "user", "The user to report", true)
+                .addOption(OptionType.STRING, "reason", "The reason for the report", false),
+
+            Commands.slash("weather", "Get the weather of a city")
+                .addOption(OptionType.STRING, "city", "The city to get the weather of", true),
+
+            Commands.slash("quote", "Get a random quote")
+                .addSubcommands(
+                    new SubcommandData("random", "Get a random quote"),
+                    new SubcommandData("start", "Start the quotes")
+                        .addOption(OptionType.CHANNEL, "channel", "The channel to start the quote in", true),
+                    new SubcommandData("stop", "Stop the quotes")
+                )
+        ).queue();
     }
 }
